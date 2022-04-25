9ce31dc116d3f93325a76cbab76aed560db6155c
gyawali9 <roshangyawali9@gmail.com>
2022-04-25 21:45:41 +0545

    sidebar styling

---
+++ src/Data/Data.js
@@ -0,0 +1,30 @@
+// sidebar icons import
+import { FiUsers } from 'react-icons/fi';
+import { VscGraph } from 'react-icons/vsc';
+import { VscDatabase } from 'react-icons/vsc';
+import { MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { BiHomeAlt } from 'react-icons/bi';
+
+// sidebar data
+export const SidebarData = [
+    {
+        icon: BiHomeAlt,
+        heading: "Dashboard"
+    },
+    {
+        icon: VscDatabase,
+        heading: "Orders"
+    },
+    {
+        icon: FiUsers,
+        heading: "Customers"
+    },
+    {
+        icon: MdOutlineProductionQuantityLimits,
+        heading: "Products"
+    },
+    {
+        icon: VscGraph,
+        heading: "Analytics"
+    }
+];
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../imgs/logo.png'
+
+import { SidebarData } from '../../Data/Data';
 import './Sidebar.css'
+
+import { FiLogOut } from 'react-icons/fi';
+
 const Sidebar = () => {
+    const [selected, setSelected] = useState(0);
   return (
     <div className="sidebar">
         {/* logo */}
         <div className="logo">
-            <img src={Logo} alt="" width={40} />
+            <img src={Logo} alt="" width={60} />
             <span>
                 Sh<span>o</span>ps
             </span>
         </div>
 
+        {/* menu */}
+        <div className="menu">
+            {SidebarData.map((item, index)=>{
+                return(
+                    <div className={selected === index? 'menuItem active' : 'menuItem' }key={index} onClick={()=>setSelected(index)}>
+                        <item.icon/>
+                        <span>
+                            {item.heading}
+                        </span>
+                    </div> 
+                )
+            })}  
+            <div className="menuItem">
+                <FiLogOut/>
+            </div> 
+        </div> 
     </div>
   )
 }
